b341177c0686297227d71aacbc92f2565ac014d1
unknown <vraj0002@student.monash.edu>
2018-05-26 19:28:43 +0800

    made final changes to throw and grenade class

--- src/starwars/actions/Throw.java
+++ src/starwars/actions/Throw.java
@@ -11,92 +11,287 @@ import starwars.entities.Grenade;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+/**
+ * Command to throw grenade.
+ *
+ * This affordance is attached to all attackable entities
+ *
+ * @author Vinitha Raj Rajagopal Muthu
+ */
+/*
+ *
+ */
 public class Throw extends SWAffordance implements SWActionInterface{
-    private ArrayList<Direction> directions;
 
-    Direction [] direction = {Grid.CompassBearing.EAST, Grid.CompassBearing.SOUTH, Grid.CompassBearing.WEST, Grid.CompassBearing.NORTHWEST,
-            Grid.CompassBearing.SOUTHEAST, Grid.CompassBearing.SOUTHWEST, Grid.CompassBearing.NORTH, Grid.CompassBearing.NORTHEAST};
-
+    /**
+     * Constructor for the <code>Attack</code> class. Will initialize the <code>messageRenderer</code> and
+     * give <code>Attack</code> a priority of 1 (lowest priority is 0).
+     *
+     * @param targetObject the target being attacked
+     * @param m message renderer to display messages
+     */
     public Throw(SWEntityInterface targetObject, MessageRenderer m){
         super(targetObject,m);
         priority=1;
-        directions = new ArrayList<Direction>(Arrays.asList(direction));
+
 
     }
+
+    /**
+     * Returns the time is takes to perform this <code>Attack</code> action.
+     *
+     * @return The duration of the Attack action. Currently hard coded to return 1.
+     */
     @Override
     public int getDuration() {
         return 1;
     }
-
+    /**
+     * Perform the <code>Throw</code> command on an entity.
+     * <p>
+     * This method performs damage (an attack) if,
+     * <ul>
+     * 	<li>The entity is in the same location as the <code>SWActor a</code> </li>
+     * 	<li>The entity is 1 step away from the <code>SWActor a</code> </li>
+     * 	<li>The entity is 2 steps away from the <code>SWActor a</code></li>
+     * </ul>
+     * <p>
+     *
+     * @author 	Vinitha Raj Rajagopal Muthu
+     * @param 	a the <code>SWActor</code> who is attacking
+     */
     @Override
     public void act(SWActor a) {
-        SWEntityInterface target = this.getTarget();
-        boolean targetIsActor = target instanceof SWActor;
-        SWActor targetActor = null;
-        if (targetIsActor) {
-            targetActor = (SWActor) target;
-        }
+
         SWEntity itemCarried = (SWEntity) a.getItemCarried();
-        /*Actor can throw a distance of 5 steps*/
-        /*if(itemCarried instanceof Grenade && targetIsActor){
-            if(a.getActorLocation().getNeighbour(directions.get(5))==targetActor.getActorLocation()){
-                targetActor.takeDamage(20);
-                itemCarried.takeDamage(20);
-            }
-            else if(a.getActorLocation().getNeighbour(directions.get(6))==targetActor.getActorLocation()){
-                targetActor.takeDamage(10);
-                itemCarried.takeDamage(10);
-            }
-            else if(a.getActorLocation().getNeighbour(directions.get(7))==(targetActor).getActorLocation()){
-                targetActor.takeDamage(5);
-                itemCarried.takeDamage(5);
-            }
-            a.say(a.getShortDescription()+" threw a grenade on "+targetActor.getShortDescription());
-        }*/
 
-        if (itemCarried != null && (a.isHumanControlled() || (targetIsActor && (a.getTeam() != targetActor.getTeam())))) {
-            SWLocation sameLocation = a.getWorld().getEntityManager().whereIs(a);
-            List<SWEntityInterface> sameLocEntities = a.getWorld().getEntitymanager().contents(sameLocation);
-            SWLocation actorNeighbourLocation1 = (SWLocation) SWAction.getEntitymanager().whereIs(a).getNeighbour(directions.get(1));
-            List<SWEntityInterface> actorNeighbourSWLocation1 = a.getWorld().getEntitymanager().contents(actorNeighbourLocation1);
-            Location actorNeighbourLocation2 = a.getWorld().find(a).getNeighbour(directions.get(2));
-            List<SWEntityInterface> actorNeighbourSWLocation2 = a.getWorld().getEntitymanager().contents((SWLocation) actorNeighbourLocation2);
-            for (int i = 0; i < sameLocEntities.size(); i++) {
-                sameLocEntities.get(i).takeDamage(20);
-            }
-            for (int j = 0; j < actorNeighbourSWLocation1.size(); j++) {
-                actorNeighbourSWLocation1.get(j).takeDamage(10);
-            }
-            for (int k = 0; k < actorNeighbourSWLocation2.size(); k++) {
-                actorNeighbourSWLocation2.get(k).takeDamage(5);
-            }
-                /*if (SWAction.getEntitymanager().whereIs(target) == a.getWorld().getEntitymanager().whereIs(a)) {
-                    target.takeDamage(20);
-                    //itemCarried.removeAffordance(this);
-                    a.say(a.getShortDescription() + " threw a grenade on 20" + target.getShortDescription());
-                } else if (a.getWorld().getEntityManager().contents()) {
 
-                    String EntitySymbol;
-                    EntitySymbol=((SWEntityInterface) a.getWorld().getEntitymanager().whereIs(a).getNeighbour(directions.get(1))).getSymbol();
-                    target.takeDamage(10);
-                    a.say(a.getShortDescription() + " threw a grenade on 10" + target.getShortDescription());
-                } else if (SWAction.getEntitymanager().whereIs(target) == a.getWorld().getEntitymanager().whereIs(a).getNeighbour(directions.get(2))) {
-                    target.takeDamage(5);
-                    a.say(a.getShortDescription() + " threw a grenade on 5" + target.getShortDescription());
+        for (int p=0;p<10;p++){
+            for (int q=0;q<10;q++) {
+                SWLocation initloc=SWAction.getEntitymanager().whereIs(a);
+                if (a.getWorld().getGrid().getLocationByCoordinates(p,q)==SWAction.getEntitymanager().whereIs(a)){
+                    //List<SWLocation> locationGuide=[];
+                    SWLocation loc= a.getWorld().getGrid().getLocationByCoordinates(p,q);
+                    List<SWEntityInterface> nearEntities = a.getWorld().getEntitymanager().contents(loc);
+                    for (int j = 0; j < nearEntities.size(); j++) {
+                        if(nearEntities.get(j)!=a) {
+                            nearEntities.get(j).takeDamage(20);
+                            a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade on the spot by " + a.getShortDescription());
+                        }
+                    }
+                    if(p<8){
+                        SWLocation loc1= a.getWorld().getGrid().getLocationByCoordinates(p+2,q);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc1);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+
+                    }
+                    if(p>1 && a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p-2,q))!=null){
+                        SWLocation loc2= a.getWorld().getGrid().getLocationByCoordinates(p-2,q);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc2);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p<8 && q<8 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p + 2, q + 2))!=null){
+                        SWLocation loc3= a.getWorld().getGrid().getLocationByCoordinates(p+2,q+2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc3);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p<8 && q>1 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p + 2, q - 2))!=null) {
+                        SWLocation loc4 = a.getWorld().getGrid().getLocationByCoordinates(p + 2, q - 2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc4);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p>1 && q>1 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p - 2, q - 2))!=null) {
+                        SWLocation loc5 = a.getWorld().getGrid().getLocationByCoordinates(p - 2, q - 2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc5);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p>1 && q<8 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p - 2, q + 2))!=null) {
+                        SWLocation loc6 = a.getWorld().getGrid().getLocationByCoordinates(p - 2, q + 2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc6);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(q<8 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p , q + 2))!=null) {
+                        SWLocation loc7 = a.getWorld().getGrid().getLocationByCoordinates(p, q + 2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc7);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(q>1 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p , q - 2))!=null) {
+                        SWLocation loc8 = a.getWorld().getGrid().getLocationByCoordinates(p, q - 2);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc8);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(10);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 2 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+
+                    if(p<9){
+                        SWLocation loc1= a.getWorld().getGrid().getLocationByCoordinates(p+1,q);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc1);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+
+                    }
+                    if(p>0 && a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p-1,q))!=null){
+                        SWLocation loc2= a.getWorld().getGrid().getLocationByCoordinates(p-1,q);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc2);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p<9 && q<9 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p + 1, q + 1))!=null){
+                        SWLocation loc3= a.getWorld().getGrid().getLocationByCoordinates(p+1,q+1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc3);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p<9 && q>0 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p + 1, q - 1))!=null) {
+                        SWLocation loc4 = a.getWorld().getGrid().getLocationByCoordinates(p + 1, q - 1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc4);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p>0 && q>0 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p - 1, q - 1))!=null) {
+                        SWLocation loc5 = a.getWorld().getGrid().getLocationByCoordinates(p - 1, q - 1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc5);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(p>0 && q<9 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p - 1, q + 1))!=null) {
+                        SWLocation loc6 = a.getWorld().getGrid().getLocationByCoordinates(p - 1, q + 1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc6);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(q<9 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p , q + 1))!=null) {
+                        SWLocation loc7 = a.getWorld().getGrid().getLocationByCoordinates(p, q + 1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc7);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+                    if(q>0 &&a.getWorld().getEntitymanager().contents(a.getWorld().getGrid().getLocationByCoordinates(p , q - 1))!=null) {
+                        SWLocation loc8 = a.getWorld().getGrid().getLocationByCoordinates(p, q - 1);
+                        nearEntities = a.getWorld().getEntitymanager().contents(loc8);
+
+                        for (int j = 0; j < nearEntities.size(); j++) {
+                            if(nearEntities.get(j)!=a) {
+                                nearEntities.get(j).takeDamage(5);
+                                a.say(nearEntities.get(j).getShortDescription() + " was attacked by a grenade 1 steps away by " + a.getShortDescription());
+                            }
+                        }
+                    }
+
+
+
+
                 }
-                a.say(a.getShortDescription() + " threw a grenade on " + target.getShortDescription());
 
-        }*/
+            }
         }
+        itemCarried.takeDamage(0);
+        itemCarried.removeAffordance(this);
+
+
+
     }
+    /**
+     * A String describing what this <code>Attack</code> action will do, suitable for display on a user interface
+     *
+     * @return String comprising "attack " and the short description of the target of this <code>Affordance</code>
+     */
     @Override
     public String getDescription() {
         return "throw "+target.getShortDescription();
     }
-
+    /**
+     * Determine whether a particular <code>SWActor a</code> can attack the target.
+     *
+     * @author 	Vinitha Raj Rajagopal Muthu
+     * @param 	a the <code>SWActor</code> being queried
+     * @return 	true any <code>SWActor</code> can always try an attack, it just won't do much
+     * 			good unless this <code>SWActor a</code> has a suitable weapon.
+     */
     @Override
     public boolean canDo(SWActor a) {
         return true;
     }
+
 }
--- src/starwars/entities/Grenade.java
+++ src/starwars/entities/Grenade.java
@@ -6,8 +6,34 @@ import starwars.SWEntity;
 import starwars.actions.Leave;
 import starwars.actions.Take;
 import starwars.actions.Throw;
-
+/**
+ * An entity that has the <code>WEAPON</code> attribute and so can
+ * be used to <code>Throw</code> on others, etc.
+ *
+ * @author 	Vinitha RAj RAjagopal Muthu
+ *
+ * @see 	{@link starwars.actions.Throw}
+ */
+/*
+ */
 public class Grenade extends SWEntity {
+    /**
+     * Constructor for the <code>Grenade</code> class. This constructor will,
+     * <ul>
+     * 	<li>Initialize the message renderer for the <code>Grenade</code></li>
+     * 	<li>Set the short description of this <code>Grenade</code> to "A Grenade"</li>
+     * 	<li>Set the long description of this <code>Grenade</code> to "A grenade!!!! BOOM DEAD!"</li>
+     * 	<li>Set the hit points of this <code>Grenade</code> to 200</li>
+     * 	<li>Add a <code>Take</code> affordance to this <code>Grenade</code> so it can be taken</li>
+     *	<li>Add a <code>WEAPON Capability</code> to this <code>Grenade</code> so it can be used to <code>Throw</code></li>
+     * </ul>
+     *
+     * @param m <code>MessageRenderer</code> to display messages.
+     *
+     * @see {@link starwars.actions.Take}
+     * @see {@link starwars.Capability}
+     * @see {@link starwars.actions.Throw}
+     */
     public Grenade(MessageRenderer m){
         super(m);
         this.longDescription="A grenade!!!! BOOM DEAD!";
@@ -18,7 +44,18 @@ public class Grenade extends SWEntity {
         this.capabilities.add(Capability.WEAPON);
         this.hitpoints=200;
     }
-
+    /**
+     * Method insists damage on this <code>Grenade</code> by removing all of its <code>hitpoints</code>
+     * <p>
+     * This method will also change this <code>Grenade</code>s <code>longDescription</code> to
+     * "Grenade has been used up and disappeared"  and this <code>Grenade</code>s <code>shortDescription</code> to
+     * "Grenade gone".
+     * <p>
+     *
+     * @author 	Vinitha Raj Rajagopal Muthu
+     * @param 	damage the amount of <code>hitpoints</code> to be reduced
+     * @see 	{@link starwars.actions.Throw}
+     */
     @Override
     public void takeDamage(int damage){
         damage=this.hitpoints;
@@ -27,7 +64,12 @@ public class Grenade extends SWEntity {
         this.longDescription="Grenade has been used up and disappeared";
         this.capabilities.remove(Capability.WEAPON);
     }
-
+    /**
+     * A symbol that is used to represent the Grenade on a text based user interface
+     *
+     * @return 	Double Character string "G!"
+     * @see 	{@link starwars.SWEntityInterface#getSymbol()}
+     */
     @Override
     public String getSymbol() {
         return "G!";
